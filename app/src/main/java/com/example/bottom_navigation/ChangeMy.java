de2b638fccb32554787994540b0c695d9010db53
Joo-sung-jun <wntjdwns6@naver.com>
2021-04-30 16:16:37 +0900

    nav bar add 어플아이콘, 전공체크리스트,개인정보수정ui

---
+++ app/src/main/java/com/example/bottom_navigation/ChangeMy.java
@@ -0,0 +1,28 @@
+package com.example.bottom_navigation;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ChangeMy extends Fragment {
+    public static ChangeMy newinstance(){
+        return new ChangeMy();
+    }
+    public ChangeMy(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_change,container,false);
+
+
+    }
+}
--- app/src/main/java/com/example/bottom_navigation/Mypage.java
+++ app/src/main/java/com/example/bottom_navigation/Mypage.java
@@ -1,48 +1,49 @@
 package com.example.bottom_navigation;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Mypage extends Fragment {
-
-    private View view;
-
-    public Mypage() {
-        // Required empty public constructor
+    public static Mypage newinstance(){    //////모든 프레그먼트에 newinstance메소드가 있어야함..!!
+        return new Mypage();
     }
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    public static Mypage newInstance(String param1, String param2) {
-        Mypage fragment = new Mypage();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public Mypage(){
+
     }
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_mypage, container, false);
+        View view = inflater.inflate(R.layout.fragment_mypage,null);
+        Button btn_change = (Button)view.findViewById(R.id.btn_change);
+        btn_change.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                ((MainActivity)getActivity()).replaceFragment(ChangeMy.newinstance());
+
+            }
+        });
         return view;
+
+
     }
+
+
 }
+
