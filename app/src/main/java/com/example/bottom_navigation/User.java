e80a4b31a1c0b309961cde0e4c6bc5c3d3ffb65c
KNS <skatjr98@naver.com>
2021-05-01 23:10:23 +0900

    DB작업

---
+++ app/src/main/java/com/example/bottom_navigation/User.java
@@ -0,0 +1,42 @@
+package com.example.bottom_navigation;
+
+public class User {
+    private String name;
+    private int credit;
+    private String area;
+    private int id;
+
+    public User(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
