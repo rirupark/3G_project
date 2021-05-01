4d1c49a0837648a1ef0f1e629f032193f2d8a7f5
Joo-sung-jun <wntjdwns6@naver.com>
2021-05-01 23:03:36 +0900

    add Calculator,Gyoyang,Passgraduation page

--- app/src/main/java/com/example/bottom_navigation/Calculator.java
+++ app/src/main/java/com/example/bottom_navigation/Calculator.java
@@ -9,39 +9,41 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Calculator extends Fragment {
 
     private View view;
+    private Spinner spinner;
+    private TextView tv_result;
 
     public Calculator() {
         // Required empty public constructor
     }
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    public static Calculator newInstance(String param1, String param2) {
-        Calculator fragment = new Calculator();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public static Calculator newInstance() {return new Calculator(); }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_calculator, container, false);
+
+        spinner = (Spinner)view.findViewById(R.id.spinner);
+        tv_result = (TextView)view.findViewById(R.id.tv_result);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                tv_result.setText(parent.getItemAtPosition(position).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         return view;
     }
 }
