ae431650ccf61ca5350313d4a24141322535a59e
Joo-sung-jun <wntjdwns6@naver.com>
2021-05-10 17:34:21 +0900

    add Calculator,Gyoyang,Passgraduation page

--- app/src/main/java/com/example/bottom_navigation/Calculator.java
+++ app/src/main/java/com/example/bottom_navigation/Calculator.java
@@ -17,8 +17,7 @@ import android.widget.TextView;
 public class Calculator extends Fragment {
 
     private View view;
-    private Spinner spinner,spinner2;
-    private TextView tv_result,tv_result2,tv_result3,tv_result4;
+
 
 
     public Calculator() {
@@ -31,43 +30,6 @@ public class Calculator extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_calculator, container, false);
-
-        spinner = (Spinner)view.findViewById(R.id.spinner);
-        spinner2 = (Spinner)view.findViewById(R.id.spinner2);
-
-
-        tv_result = (TextView)view.findViewById(R.id.tv_result);
-        tv_result2 = (TextView)view.findViewById(R.id.tv_result2) ;
-
-
-
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                tv_result.setText(parent.getItemAtPosition(position).toString());
-                ((TextView)parent.getChildAt(0)).setTextColor(Color.WHITE);
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                tv_result2.setText(parent.getItemAtPosition(position).toString());
-                ((TextView)parent.getChildAt(0)).setTextColor(Color.WHITE);
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
 
 
         return view;
---
+++ app/src/main/java/com/example/bottom_navigation/Listjeong.java
@@ -0,0 +1,18 @@
+package com.example.bottom_navigation;
+
+public class Listjeong {
+
+    private String jeongongName;
+    public Listjeong(){}
+
+    public String getJeongongName() {
+        return jeongongName;
+    }
+
+    public void setJeongongName(String jeongongName) {
+        this.jeongongName = jeongongName;
+    }
+}
+
+
+
