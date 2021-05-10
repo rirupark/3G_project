6c8b1be31b039917259ed4c0b2fe487869bb8f0d
minju park <mlnjv016@gmail.com>
2021-05-11 02:55:10 +0900

    체크리스트 전공 레이아웃 수정

--- app/src/main/java/com/example/bottom_navigation/Jeongong.java
+++ app/src/main/java/com/example/bottom_navigation/Jeongong.java
@@ -14,18 +14,22 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class Jeongong extends Fragment {
 
     private View view;
-    private Spinner spn_jeongong,spn_grade;
-    private TextView tv_result3,tv_result4;
+    private Spinner spn_jeongong, spn_grade;
+    private TextView tv_result3, tv_result4;
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
@@ -35,10 +39,11 @@ public class Jeongong extends Fragment {
     private DatabaseReference databaseReference;
 
 
-    public static Jeongong newinstance(){
+    public static Jeongong newinstance() {
         return new Jeongong();
     }
-    public Jeongong(){
+
+    public Jeongong() {
 
     }
 
@@ -46,7 +51,46 @@ public class Jeongong extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-     view =  inflater.inflate(R.layout.fragment_jeongong,container,false);
+        view = inflater.inflate(R.layout.fragment_jeongong, null);
+        recyclerView = view.findViewById(R.id.re_jeongong2);//아이디 연결
+        recyclerView.setHasFixedSize(true);//리사이클러뷰 기존성능강화
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>();//User 객체를 담을 어레이 리스트(어댑터쪽으로)
+
+        database = FirebaseDatabase.getInstance(); // 파이어베이스 데이터베이스 연동
+        databaseReference = database.getReference("User"); // DB테이블 연결
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //파이어베이스 데이터베이스의 데이터를 받아오는 곳
+                arrayList.clear(); // 기존 배열리스트가 존재하지 않게 초기화(방지차원)
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    User user = snapshot.getValue(User.class); // 만들어둔 User 객체에 데이터를 담는다.
+                    arrayList.add(user); //담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼준비
+
+
+                }
+                adapter.notifyDataSetChanged();  // 리스트 저장 및 새로고침
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //디비를 가져오던중 에러발생시
+
+
+            }
+        });
+        adapter = new CustomAdapter(arrayList, getActivity());
+        recyclerView.setAdapter(adapter); //리사이클러뷰에 어댑터연결
+
+
+        return view;
+    }
+}
+
+
 
 
 
@@ -87,11 +131,8 @@ public class Jeongong extends Fragment {
 
 
 
-            return view;
-    }
 
 
 
 
 
-}
