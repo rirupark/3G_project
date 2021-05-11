0de62f9e467427c7b6ae3861f6cf3ba56c96f716
minju park <mlnjv016@gmail.com>
2021-05-12 00:44:29 +0900

    졸업 인증제 레이아웃 디자인 + 뒤로가기 버튼 추가

--- app/src/main/java/com/example/bottom_navigation/PassgraEss.java
+++ app/src/main/java/com/example/bottom_navigation/PassgraEss.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
 public class PassgraEss extends Fragment {
@@ -23,6 +24,13 @@ public class PassgraEss extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_passgra_ess,null);
+        ImageButton btn_back = (ImageButton)view.findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity)getActivity()).replaceFragment(Passgraduation.newinstance());
+            }
+        });
 
         return view;
 
